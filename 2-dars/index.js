7790731062ee8a0de7bfc1e828ba4283a895c74a
murodillo-dev <murodilloxakker@gmail.com>
2024-04-03 16:05:44 +0500

    .

---
+++ 2-dars/index.js
@@ -0,0 +1,75 @@
+// console.log("welcome \nmeta it school");
+// console.log('welcome \nmeta it school');
+// console.log(`welcome meta it school`);
+
+// console.log("o'quvchi");
+
+// let a = 15
+// console.log(a);
+// console.log('a');
+
+// console.log(9007199254740992);
+// console.log(9007199254740992n + 10000n);
+
+// let a = 5
+// let b = 8
+// console.log(a < b);
+
+let a = null
+let b
+// console.log(a + 1);
+// console.log(b + 1);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(a === b);
+
+let son1 = 5
+let son2 = '5'
+console.log('number' === 'string');
+
+let key1 = Symbol('salom')
+let key2 = Symbol('salom')
+// let a = 'aasa'
+console.log(key1 == key2);
+// NaN not a number
+
+
+let array = ['Ali', true, false, undefined, null]
+console.log(array);
+
+let object = { ism: 'Ali', familia: 'aliyev', yosh: 24 }
+console.log(object);
+
+// let hello = ()=>{
+//     console.log('hello world');
+//     console.log('salom dunyo');
+// }
+// hello()
+// hello()
+// hello()
+
+let num1 = 1
+let num2 = '1'
+// console.log(num1+ +num2);
+
+// console.log(isNaN(num1));
+// console.log(isNaN(+num2));
+
+let son3 = '5'
+console.log(typeof parseFloat(son3));
+console.log(parseInt(son3));
+console.log(Number(son3));
+
+let user1 = { ism: 'ali', id: 1 }
+let link = '54d54sadsad4d6ad'
+
+let user2 = { ism: 'ali', id: 1 }
+let link2 = 'sdasd456asdsa56d'
+console.log(user1 == user2);
+
+// let rost = true => 1
+// let yolgon = false => 0, null , undefined
+
+// console.log(true != null);
+// console.log(6 != 6);
