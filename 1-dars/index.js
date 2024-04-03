42dedf4f2b759cd54e04897d576eaebb69bdc61a
murodillo-dev <murodilloxakker@gmail.com>
2024-04-03 11:44:04 +0500

    .

---
+++ 1-dars/index.js
@@ -0,0 +1,51 @@
+// console.log('hello world');
+// let text = 'Lorem ipsum dolor 
+
+// console.log(text);
+
+// let ism1 = 'Eshmat'
+// var ism2 = 'Toshmat'
+// var ism2 = 'Eshmat'
+// ism2 = 'Xusan'
+// console.log(ism2);
+// const ism3 = 'Teshavoy'
+
+// console.log(ism1);
+// ism1 = 'Vali'
+// console.log(ism1);
+
+// console.log(ism3);
+// ism3 = 'Xasanboy'
+// console.log(ism3);
+
+// string=>matnlar
+// Number=>raqam 
+// Boolean=> true , false
+// undefined
+
+// let matn 
+// console.log(matn);
+
+let son1 = 5
+let son2 = 15
+// let natija = 
+// console.log(son1 % son2);
+// son1++
+// son1+=15
+// son1-=15
+// son1*=3
+// son1/=3
+// son1 = son1/3
+// let result = son1**3
+// console.log(result);
+
+let study = 'Meta it school'
+let feild = 'frontend'
+
+console.log(study + ' ' + feild);
+console.log(`${study} ${feild}`);
+
+alert('hello \n world')
+
+meta_murodillo
+
