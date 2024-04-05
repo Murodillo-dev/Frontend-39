9d270885c7d08e3c16e12faecb9876463180ef58
murodillo-dev <murodilloxakker@gmail.com>
2024-04-05 15:55:00 +0500

    3-dars ternary operator

---
+++ index.js
@@ -0,0 +1,56 @@
+// let son=5
+// let son2 = 2
+// let natija = Math.pow(son ,son2)
+// console.log(natija);
+
+// console.log(Math.sqrt(39));
+
+// console.log(Math.floor(-5.9));
+// console.log(Math.ceil(-5.9));
+// console.log(Math.round(-3.99));
+// console.log(Math.trunc(-5.9));
+// console.log(Math.trunc(Math.random()*1000));
+// let son = 123
+// console.log(typeof String(son));
+
+// let number = 898
+// let birlik = number % 10
+// let onlik = Math.trunc(number / 10) % 10
+// let yuzlik = Math.trunc(number / 100)
+// //
+// // console.log(String(birlik) + String(onlik) + String(yuzlik));
+// console.log(number);
+// console.log(birlik * 100 + onlik * 10 + yuzlik);
+
+
+// && *
+// || +
+// ! inkor
+
+// console.log(!true);
+// true && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+
+// !true = false
+// !false = true
+
+// let ielts = 6.5
+// let natija = (ielts>6.5) ?  true : false
+// let natija = (ielts >= 6.5) ? "Amerikaga ketsin" : "UZB da qolsin"
+// console.log(natija);
+
+// let yosh = 2
+// let result = (yosh >= 3 && yosh <= 10) ? "parkka kirish tekin" : "parkka kirish 15 ming so'm"
+// console.log(result);
+
+// let baxo = 4
+// let ielts = 5
+// let result = (baxo >= 4 || ielts >= 6) ? "Grant" : "Kantrakt"
+// console.log(result); 
