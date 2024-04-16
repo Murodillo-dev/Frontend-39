ed9a132ca788c0e8d602b4d12c1f5a036716ae1c
murodillo-dev <murodilloxakker@gmail.com>
2024-04-16 10:38:54 +0500

    If else shart operatori

---
+++ 5-dars/index.js
@@ -0,0 +1,93 @@
+// let text = 'meta,it,school'
+// console.log(text.includes('meta'));
+// console.log(text.replace('school','maktab'));
+// console.log(text[2]);
+// console.log(text.charAt());
+// console.log(text.at();)
+// console.log(text.slice(0,4));
+// console.log(text.slice(2));
+// console.log(text.padEnd(17,'.'));
+// console.log(text.padStart(17,'.'));
+// console.log(text.padStart(17,'.'));
+// console.log(text.endsWith('it school'));
+// console.log(text.indexOf('it'));
+// console.log(text.split('t'));
+
+// let temp = false
+
+// if (false) {
+//   console.log("tashqarida yomgir yogyapti");
+// } else {
+//   console.log("tashqari quyoshli");
+// }
+
+// if(true){
+//     console.log('tashqarida yomgir yogyapti');
+// }
+
+// let age = prompt('yoshni kiriting')
+// if (age>=16) {
+//     console.log('passport berilsin');
+// }
+// else{
+//     console.log('passport berilmasin');
+// }
+
+// let temp = issiq, sovuq , juda issiq ,  juda sovuq
+
+// issiq=> 10-25
+// juda issiq => 26-50
+// sovuq=>0- (-10)
+// juda sovuq=> (-11) - (-20)
+
+// let havo = prompt("gradus");
+
+// if (havo > 26) {
+//   console.log("havo juda issiq");
+// } else if (havo > 10) {
+//   console.log("havo issiq");
+// } else if (havo <= 0) {
+//   console.log("havo sovuq");
+// } else if (havo <= -11) {
+//   console.log("havo juda sovuq");
+// } else {
+//   console.log("error");
+// }
+
+// if (havo >= 26 && havo <= 50) {
+//   console.log("havo juda issiq");
+// } else if (havo >= 1 && havo <= 25) {
+//   console.log("havo issiq");
+// } else if (havo <= 0 && havo >= -10) {
+//   console.log("havo sovuq");
+// } else if (havo <= -11) {
+//   console.log("havo juda sovuq");
+// }
+// else{
+//     console.log('bunaqa gradus yoq');
+// }
+
+// let familiya = prompt("familiya");
+
+// if (familiya.endsWith("v")) console.log("male");
+// else if (familiya.endsWith("va")) console.log("female");
+// else console.log("there is no such familiya");
+
+// login = admin
+// parol = 1234
+
+// let login = prompt("login");
+// let parol = prompt("parol");
+
+// if (login == "admin" && parol == 1234) console.log("loged In");
+// else if (login == "admin" && parol != 1234) console.log("parolda xatolik bor");
+// else if (login != "admin" && parol == 1234) console.log("loginda xatolik bor");
+// else console.log("ERROR");
+
+// let son1 = prompt('son1')
+// let son2 = prompt('son2')
+// let amal = prompt('+\n-\n*\n/')
+
+// if (amal == '-') {
+    
+// }
