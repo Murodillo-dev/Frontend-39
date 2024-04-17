52b47717d9e444f16b3405fe471b770397653ace
murodillo-dev <murodilloxakker@gmail.com>
2024-04-17 17:26:34 +0500

    for loop

---
+++ 6-dars/index.js
@@ -0,0 +1,126 @@
+// let son = -5
+// if (son>0) {
+//     son++
+//     console.log(son);
+// }
+// else{
+//     console.log(son);
+// }
+
+// let oy = 14;
+// if (oy >= 3 && oy <= 5) {
+//   console.log("bahor");
+// } else if (oy >= 6 && oy <= 8) {
+//   console.log("yoz");
+// } else if (oy >= 9 && oy <= 11) {
+//   console.log("kuz");
+// } else if (oy == 12) {
+//   console.log("qish");
+// } else if (oy >= 1 && oy <= 2) {
+//   console.log("qish");
+// } else {
+//   console.log("bunday fasl yoq");
+// }
+
+// let sum = prompt("so'mni kiriting")
+// let valyuta = prompt("yevro\ndollar")
+// if (valyuta=='yevro') {
+//     console.log(Math.trunc(sum/13600));
+// }
+// else if (valyuta == 'dollar') {
+//     console.log(Math.trunc(sum/12600));
+// }
+// else{
+//     console.log("error");
+// }
+
+// let kun = 2;
+// if (kun == 1) {
+//   console.log("dushanba");
+// }
+// if (kun == 2) {
+//   console.log("seshanba");
+// }
+// if (kun == 3) {
+//   console.log("chorshanba");
+// } else {
+//   console.log("bunday kun yoq");
+// }
+
+// for(start; direction; step )
+
+let toq = 0;
+// let juft = 0;
+// let count = 0
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     juft++;
+//   }
+//   if (i % 2 != 0) {
+//     toq++;
+//   }
+//   count++
+// }
+
+// console.log(toq);
+// console.log(juft);
+// console.log(count);
+
+// for (let i = 0; i < 15; i++) {
+//   if (i == 5) break;
+//   console.log(i);
+// }
+
+// for (let i = 0; i < 15; i++) {
+//   if (i >= 5 && i<=10) continue;
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 15; i++) {
+//     console.log(i);
+//     i+=i
+// }
+// let count1 = 0;
+// let count2 = 0;
+// let count3 = 0;
+// for (let i = 1; i <= 10; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     for (let f = 1; f <= 10; f++) {
+//       count1++;
+//     }
+//     count2++;
+//   }
+//   count3++;
+// }
+
+// console.log(count1);
+// console.log(count2);
+// console.log(count3);
+
+
+for (let i = 1; i <=10; i++) {
+   for (let j = 1; j <=10; j++) {
+    console.log(2*j);
+   }
+}
+
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+
+
+
+// 1 karra 
+// 1x1 = 1
+// 1x2 = 2
+// 1x3 = 3
+// ==========
+// 2 karra 
+// 2x1 = 2
+// 2x2 = 4
+// 2x3 = 6
