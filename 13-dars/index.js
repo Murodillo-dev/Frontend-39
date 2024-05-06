b3b2b8602e002f9fda331cf485c834adcd46325c
murodillo-dev <murodilloxakker@gmail.com>
2024-05-06 09:29:34 +0500

    birinchi oy imtixon

---
+++ 13-dars/index.js
@@ -0,0 +1,34 @@
+//1) frontend....
+// 2) ----frontend
+// 3) Bigint ⇒ 9007199254740992 shu songa 2 ni qoshing
+
+// 4)let son = 5.2  
+// 4.1)shu sonni butun qismini chiqaring => result:5
+// 4.2) shu sonni yaxlitlab chiqaring => result:5
+// 4.3) shu sonni bitta tepasini chaqring=> result:6
+
+// 5) bitta tasodifiy son chiqaring 0-100 ni orasida bo'lsin 
+// 6)'Meta it school' shu text dan 'h' shu harfni 2 xil usulda kesib olib ekranga chiqaring
+// 7)'Meta it school' shu textni uzunligini aniqlang
+// 8)'Meta it school' shu textdan "it" so'zini nechanchi o'rinda turganini aniqlang
+// 9)'Meta it school' shu textni orasidagi bo'sh joylarni olib tashlang
+// 10)'Meta it school' shu textdan 'school' so'zini maktab so'ziga almashtirib bering 
+// 11)'Meta it school' shu textni ichida 'Meta' so'zi bor yoki yoqligini aniqlang
+// 12)3 ta son berilgan agar shu sonlarni ichida manfiy sonlar bolsa manfiy degan arrayga qo'shing agar musbat sonlar bolsa musbat degan arrayga qo'shing
+
+// 13)1 dan 100 gacha bolsan sonlarni ekranga chiqaring
+// 13.1)shu sonlar orasida juftlarini va toqlarini ikkita arrayga yig'ing
+// 13.2) son 50 ga yetganda for loop to'xtab qolsin
+// 13.3) 1-100 orasida faqat 3 ga bo'linadigon sonlar kelib qolsa ularni ekranga chiqarmasdan tashlab ketsin
+
+// 14) t_yilni kiritsa yoshni xisoblab beradigon function yarating 3 xil korinishda bo'lsin va yil parametr sifatida berilsin
+// 15) function ism va familiya parametr sifatida berilsin agar ism va familiya berilmasa function default parametr olsin => 'eshmat','toshmat'
+// 16)3 ta talaba bor ularni 4ta fandan baxolari bor xar bir talabani o'rtacha balini xisoblash kerek va 3 talabani ham umumiy balini xisoblash kerek function yordamida
+
+// 17) Object yarating ichida keys=> 'name','surname','full name' bolsin va shularni ekranga chiqaring
+// 17.1) Object age va adress qo'shing
+// 17.2) Object ichidagi keylarni ekranga chiqaring
+// 17.3) Object dan nusxa oling
+// 17.4) nested Object yarating va shu Object destructure qilib bering
+// 17.5) Object qiymatlarini spread operatori bilan yangilang
+// 17.6) Object ichida function yarating va this ni ishlatilishini korsatib bering
